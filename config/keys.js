8230739b3425abbbfb420b8fb089a35890d2b319
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-11-30 19:21:19 +0000

    feat: create login route, implement the login checking email/passwd functionality and create a token once user's credentials are matched

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://gerry:gerry86@ds137089.mlab.com:37089/mern-devconnector"
+  mongoURI: "mongodb://gerry:gerry86@ds137089.mlab.com:37089/mern-devconnector",
+  secretOrKey: 'secret'
 };
