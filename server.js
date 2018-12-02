d331011b459c8e0b26ed63679c098e789f1f7f59
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-12-02 18:04:33 +0000

    prepared for deploy

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser');
-const passport = require('passport');
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -10,7 +11,7 @@ const posts = require("./routes/api/posts");
 const app = express();
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB config
@@ -26,12 +27,22 @@ mongoose
 app.use(passport.initialize());
 
 // Passport Config
-require('./config/passport.js')(passport);
+require("./config/passport.js")(passport);
 
 // Use Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// Serve static if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
