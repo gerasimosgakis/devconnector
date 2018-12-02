a73b50ea0616fc8260465ab36b19ce10c4105dd0
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-12-02 17:40:17 +0000

    feat: add different key files for dev and for prod and add an if statement in the main keys file so we use the correct keys depending on the mode

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://gerry:gerry86@ds137089.mlab.com:37089/mern-devconnector",
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
